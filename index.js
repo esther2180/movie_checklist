79ecc915650b7c91611bfc3f59066424d8668641
Esther <esther2180@gmail.com>
2019-01-16 11:24:44 -0800

    initial commit with images

---
+++ index.js
@@ -0,0 +1,23 @@
+// $(document).ready(initializeApp);
+
+// function initializeApp() {
+// createBoard(imagesArray);
+// }
+
+// var imagesArray = [
+// "images/1_a_new_hope.png", // 1st image
+// "images/2_the_empire_strikes_back.png",
+// "imgaes/3_return_of_the_jedi.png",
+// "images/4_the_phantom_menace.png",
+// "images/5_attack_of_the_clones.png",
+// "images/6_revenge_of_the_sith.png",
+// "images/7_the_force_awakens.png",
+// "images/8_star_wars_episode_IX.png" // 8th image
+// ];
+
+// function ShowHideDiv(checked_movie) {
+// var watched_movie = document.getElementById("watched_movie");
+// watched_movie.style.display = checked_movie.checked ? "block" : "block";
+// }
+
+
